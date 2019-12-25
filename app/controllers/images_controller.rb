58448bea6bf1b99d55f46b5bc9e5e9d14eeac6fb
Turkhan Badalov <turkhan.badalov@ut.ee>
2019-12-26 01:59:51 +0200

    Allow only creating and viewing of images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,29 +1,9 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
 
-  # GET /images
-  # GET /images.json
-  def index
-    @images = Image.all
-    @images.each do |image|
-        image.file.purge
-    end
-    Image.destroy_all
-    []
-  end
-
   # GET /images/1
   # GET /images/1.json
   def show
-  end
-
-  # GET /images/new
-  def new
-    @image = Image.new
-  end
-
-  # GET /images/1/edit
-  def edit
   end
 
   # POST /images
@@ -40,30 +20,6 @@ class ImagesController < ApplicationController
         format.html { render :new }
         #format.json { render json: @image.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /images/1
-  # PATCH/PUT /images/1.json
-  def update
-    respond_to do |format|
-      if @image.update(image_params)
-        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
-        format.json { render :show, status: :ok, location: @image }
-      else
-        format.html { render :edit }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /images/1
-  # DELETE /images/1.json
-  def destroy
-    @image.destroy
-    respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :images, param: :control_hash
+  resources :images, only: [:create, :show], param: :control_hash
   root 'main#index'
 end
